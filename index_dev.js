2701d157c6f051f9e068c12811089ecf37837319
Andy Kim <andy.chiwon.kim@gmail.com>
2023-10-17 06:35:09 +0900

    wip

--- index_dev.js
+++ index_dev.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const http = require('http');
+const path = require("path")
 
 const app = express();
-app.set('view engine', 'ejs');
 http.createServer(app).listen(3000);
 
+app.set('view engine', 'ejs');
+app.use(express.static('public')); 
+app.use(express.urlencoded({extended:true})); 
+
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap-icons')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+
+app.get("/about", (req, res) => {
+    res.render('about')
+})
 
 app.get("/", (req, res) => {
-    const buldogs = [
-        {a: 1, b: 2},
-        {c: 1, d: 2},
-        {e: 1, f: 2},
-    ]
-    console.log(req.url, req.method);
-    res.render('temp', {buldogs})
+    res.render('main', {})
 })
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    console.log(req.body.search_input)
+    res.render('main', {code: req.body.search_input})
+})
+
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
