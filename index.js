dc13fe91a8e7ed3c4becdac0ad3aab109d80503d
Andy Kim <andy.chiwon.kim@gmail.com>
2023-10-17 06:43:31 +0900

    wip

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
+const path = require("path")
 
 let local_settings = JSON.parse(fs.readFileSync('local_settings.json'));
 const options = {
@@ -12,28 +13,46 @@ const options = {
 // Create a service (the app object is just a callback).
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.static('public')); 
+app.use(express.urlencoded({extended:true})); 
 
 // Create an HTTP service.
 http.createServer(app).listen(80);
 // Create an HTTPS service identical to the HTTP service.
 https.createServer(options, app).listen(443, () => {
-  console.log(`Example app listening on port 443`)
+  console.log('HTTPS')
 }); 
 
-app.get("*", (req, res) => {
-    console.log("req.secure == " + req.secure);
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap-icons')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 
+app.get("*", (req, res, next) => {
+    console.log("req.secure == " + req.secure);
     if(req.secure){
         // --- https
-        // res.send('TN Partners --- to be developed soon. ')
-        res.render('temp')
+        console.log('pass')
+        next()
     }else{
         // -- http
-        let to = "https://" + req.headers.host + req.url;
-        console.log("to ==> " + to);
-
         return res.redirect("https://" + req.headers.host + req.url);
     }
 })
-// app.get('/', (req, res) => {
-//});
+
+app.get("/about", (req, res) => {
+    res.render('about')
+})
+
+app.get("/", (req, res) => {
+    res.render('main', {})
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    console.log(req.body.search_input)
+    res.render('main', {code: req.body.search_input})
+})
+
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
