8401c65f9d5f790254ebf6e7afd8616bb8069b29
Andy Kim <andy.chiwon.kim@gmail.com>
2024-02-18 11:23:34 +0900

    wip

--- index_dev.js
+++ index_dev.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require("path");
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 
 http.createServer(app).listen(3000);
@@ -19,7 +20,18 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    res.render('main', {});
+    const db = new sqlite3.Database('df_krx.db');
+    const keyword = req.query.keyword || '삼성'; // Get the keyword from the query parameters
+    const query = `SELECT * FROM krx_data WHERE Name LIKE '%${keyword}%'`;
+
+    db.all(query, [], (err, rows) => {
+        if (err) {
+        throw err;
+        }
+
+        res.render('main', { rows });
+    });
+    db.close()
 });
 
 app.post("/", (req, res) => {
