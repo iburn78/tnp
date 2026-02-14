4bf685ca5e297fc5ab923fdc5c85345849133a44
andy <andy.chiwon.kim@gmail.com>
2026-02-14 16:11:23 +0900

    wip

--- routes/tnpartners.js
+++ routes/tnpartners.js
@@ -36,22 +36,6 @@ router.get("/andy_d", (req, res) => {
     });
 });
 
-router.get("/andy_p", (req, res) => {
-    const filePath = 'public/andy/periodic_update.json'
-    
-    // Read the JSON file
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error("Error reading the file:", err);
-            res.status(500).send("Error loading data.");
-        } else {
-            const jsonData = JSON.parse(data);
-            // Pass jsonData to the EJS template
-            res.render('tnpartners/periodic_update', { report: jsonData });
-        }
-    });
-});
-
 router.use((req, res) => {
     res.status(404).render('tnpartners/404', { title: '404' });
 });
