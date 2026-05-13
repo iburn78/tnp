b7933bf7bd52fc35b5b32d168606e3b874ae3d60
andy <andy.chiwon.kim@gmail.com>
2026-05-13 14:59:23 +0900

    wip

--- routes/tnpartners.js
+++ routes/tnpartners.js
@@ -19,23 +19,6 @@ router.get("/about", (req, res) => {
     res.render('tnpartners/about');
 });
 
-
-router.get("/andy_d", (req, res) => {
-    const filePath = 'public/andy/daily_update.json'
-    
-    // Read the JSON file
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error("Error reading the file:", err);
-            res.status(500).send("Error loading data.");
-        } else {
-            const jsonData = JSON.parse(data);
-            // Pass jsonData to the EJS template
-            res.render('tnpartners/daily_update', { report: jsonData });
-        }
-    });
-});
-
 router.use((req, res) => {
     res.status(404).render('tnpartners/404', { title: '404' });
 });
